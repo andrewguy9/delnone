39f1e3d348181e01c783369d012c9ed12641f1e9
Andrew Thomson <athomsonguy@gmail.com>
2016-04-27 17:31:18 -0700

    remove weird assert

--- delnone/__init__.py
+++ delnone/__init__.py
@@ -7,7 +7,6 @@ def delnone(i):
   if isinstance(i, dict):
     o = {}
     for (key, value) in i.items():
-      assert isinstance(key, basestring)
       value = delnone(value)
       if value is not None:
         o[key] = value
