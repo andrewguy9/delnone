190ec6d9085bd0a5ec07332ead66d5881599780b
Andrew Thomson <athomsonguy@gmail.com>
2018-10-08 14:27:57 -0700

    Fix list comprehension.

--- delnone/__init__.py
+++ delnone/__init__.py
@@ -6,7 +6,7 @@ def delnone(i):
   if isinstance(i, dict):
     o = {key:delnone(value) for (key,value) in i.items() if value is not None}
   elif isinstance(i, list):
-    o = [value for delnone(value) in i if value is not None]
+    o = [delnone(value) for value in i if value is not None]
   else:
     o = i
   return o
